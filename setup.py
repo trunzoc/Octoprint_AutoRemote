6cc8cde4e24caacc639afd54ff68f183e63826fb
trunzoc <trunzoc@users.noreply.github.com>
2018-09-27 09:00:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@
 plugin_identifier = "OctoRemote"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_Autoremote"
+plugin_package = "octoprint_AutoRemote"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint_Autoremote"
+plugin_name = "OctoPrint_AutoRemote"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "1.0.0"
