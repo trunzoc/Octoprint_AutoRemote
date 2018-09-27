660db3917ef4b612b9fb67be754a648808f05803
trunzoc <trunzoc@users.noreply.github.com>
2018-09-27 17:20:13 -0400

    Update __init__.py

--- octoprint_AutoRemote/__init__.py
+++ octoprint_AutoRemote/__init__.py
@@ -78,7 +78,7 @@ class OctoAutoremotePlugin(octoprint.plugin.StartupPlugin,
 	
             if not payload:
                 payload = ["no_payload"]
-		message += ",No_Message"
+                message += ",No_Message"
 		
             if 'remoteAddress' in payload:
                 message += ",RemoteAddress:" + payload["remoteAddress"]
